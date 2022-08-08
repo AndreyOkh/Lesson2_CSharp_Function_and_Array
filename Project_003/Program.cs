5b00501273c822ce06af127a05b82c3e466c7c1b
Андрей Охотников <andrey1q2q@gmail.com>
2022-08-08 10:17:08 +1000

    Project_003 Написал программу которая в консоль получает слово и выводит его индекс в массиве

---
+++ Project_003/Program.cs
@@ -0,0 +1,17 @@
+﻿//Программа получает в консоль слово для поиска и выдает индекс искомого изначения если находит его в массиве
+
+string[] array = {"Ivan", "Andrey", "Michail", "Natalia", "Maria", "Sergey", "Dmitry"};
+
+Console.Write("Введите имя: ");
+string find = Console.ReadLine();
+
+int n = array.Length;
+int index = 0;
+
+while (index < n) {
+    if (array[index] == find) {
+        Console.WriteLine($"Индекс имени {find} в массиве {index}");
+        break;
+    }
+    index++;
+}
